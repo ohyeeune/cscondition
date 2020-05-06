677b0f2fafd25df6e05d8397305f50e1bf8d2a66
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-06 14:42:59 +0900

    13-5 switch 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,12 +15,24 @@ namespace cscondition
             Console.Write("숫자 입력 : ");
             int input = int.Parse(Console.ReadLine());
 
+            /*
             if (input % 2 == 0)
             {
                 Console.WriteLine("짝수입니다!");
             }else
             {
                 Console.WriteLine("홀수입니다!");
+            }
+            */
+
+            switch(input %2)
+            {
+                case 0:
+                    Console.WriteLine("짝수입니다!");
+                    break;
+                case 1:
+                    Console.WriteLine("홀수입니다!");
+                    break;
             }
 
             // 현재 시간 구하기
@@ -42,6 +55,35 @@ namespace cscondition
             else
             {
                 Console.WriteLine("저녁 먹을 시간입니다.");
+            }
+
+            Console.Write("이번 달은 계절이 무엇인가요? : ");
+            int input2 = int.Parse(Console.ReadLine());
+            switch(input2)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다.");
+                    break;
+                case 9:
+                case 10 :
+                case 11:
+                    Console.WriteLine("가을입니다.");
+                    break;
+                default:
+                    Console.WriteLine("대체 어느 행성에 살고 계신가요?");
+                    break;
             }
         }
     }
